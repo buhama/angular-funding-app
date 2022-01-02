066937534f1c296b90d88ed5b3898402b03f1eab
buhama <hassan@hassanahmed.ca>
2022-01-02 01:19:43 -0500

    started and connected bootstrap

---
+++ src/app/components/info/club-info/club-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClubInfoComponent } from './club-info.component';
+
+describe('ClubInfoComponent', () => {
+  let component: ClubInfoComponent;
+  let fixture: ComponentFixture<ClubInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClubInfoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClubInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
